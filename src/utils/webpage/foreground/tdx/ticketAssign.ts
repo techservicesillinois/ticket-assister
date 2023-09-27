fb4fb0e21e1e2e718d6e3520305e6efb82744815
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-09-27 18:35:11 -0500

    Implement rule
    - add helper to display notice
    - correct behavior of getTicketDatumBg function
    - write CS logic
    - local testing of functionality succeeded
    
    Resolves #10

--- src/utils/webpage/foreground/tdx/ticketAssign.ts
+++ src/utils/webpage/foreground/tdx/ticketAssign.ts
@@ -26,3 +26,27 @@ export function getNewResponsibilityEl() {
 	}
 	return el;
 }
+/**
+ * Displays a notice box
+ * on the Ticket Reassign screen
+ */
+export function showNotice(message: string) {
+	const messageEl = document.createElement("p");
+	messageEl.style.backgroundColor = "#a3ccf5";
+	messageEl.style.color = "#000";
+	messageEl.style.padding = "1em";
+	messageEl.style.borderRadius = "1em";
+	const leadEl = document.createElement("span");
+	leadEl.textContent = "Notice: ";
+	leadEl.style.fontWeight = "600";
+	messageEl.appendChild(leadEl);
+	const contentsEl = document.createTextNode(message);
+	messageEl.appendChild(contentsEl);
+	
+	const appendAfter = (() => {
+		const pot = document.querySelector("form .container-fluid .gutter-top");
+		if (pot === null) return document.body;
+		return pot;
+	})();
+	appendAfter.appendChild(messageEl);
+}
