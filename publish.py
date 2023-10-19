24897b6a86352da78995a18c9dedec5a0a4d5964
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-10-19 10:49:45 -0500

    Work on oauth in python script

--- publish.py
+++ publish.py
@@ -9,16 +9,36 @@ import sys
 # todo: provide oauth client id to google api
 # and store client secret/token
 
+# curl "https://accounts.google.com/o/oauth2/token" -d \
+# "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$CODE&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
+result = requests.post('https://accounts.google.com/o/oauth2/token',
+    data={
+            'client_id': os.environ["CHROME_CLIENT_ID"],
+            'client_secret': os.environ["CHROME_CLIENT_SECRET"],
+            'code': os.environ["CHROME_CODE"],
+            'grant_type': 'authorization_code',
+            'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob'
+        }
+    )
+
+if result.status_code != requests.codes.ok:
+    print(f'Error: {result.text}')
+    sys.exit(1)
+
+#result_json = 
+import pdb; pdb.set_trace()
+access_token = result['access_token']
+
+'''
 file_contents = open(sys.argv[1], 'rb').read()
 encoded_contents = base64.b64encode(file_contents)
 payload = encoded_contents
-headers = {'ph-auth-token': os.environ['SOAR_TOKEN']}
 hostname = os.environ['SOAR_HOSTNAME']
-result = requests.post(f'https://{hostname}/rest/app',
-                       headers=headers,
+result = requests.post('https://accounts.google.com/o/oauth2/token',
                        data=payload)
 
 print(result.text)
 
 if result.status_code != requests.codes.ok or 'failed' in result.json():
     sys.exit(1)
+'''
