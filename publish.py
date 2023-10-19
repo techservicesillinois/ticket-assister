9166de91800e4dc600a9834052427e240f60915b
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-10-19 10:52:16 -0500

    Clean up and note where left off
    - add reference doc

--- publish.py
+++ publish.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 import base64
-import json
 import os
 import requests
 import sys
 
-# todo: provide oauth client id to google api
-# and store client secret/token
+# https://developer.chrome.com/docs/webstore/using_webstore_api/
 
 # curl "https://accounts.google.com/o/oauth2/token" -d \
 # "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$CODE&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
@@ -25,15 +23,27 @@ if result.status_code != requests.codes.ok:
     print(f'Error: {result.text}')
     sys.exit(1)
 
-#result_json = 
 import pdb; pdb.set_trace()
-access_token = result['access_token']
+# todo: check that this is working
+result_json = result.json()
+access_token = result_json['access_token']
+
+# TODO: upload using access_token
+# according to the following CURL:
+'''
+curl \
+    -H "Authorization: Bearer $TOKEN"  \
+    -H "x-goog-api-version: 2" \
+    -X PUT \
+    -T $FILE_NAME \
+    -v \
+    https://www.googleapis.com/upload/chromewebstore/v1.1/items/$ITEM_ID
+'''
 
 '''
 file_contents = open(sys.argv[1], 'rb').read()
 encoded_contents = base64.b64encode(file_contents)
 payload = encoded_contents
-hostname = os.environ['SOAR_HOSTNAME']
 result = requests.post('https://accounts.google.com/o/oauth2/token',
                        data=payload)
 
