4ade6392b8f909908671789ca9fba7e2f77698e6
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-10-23 10:42:54 -0500

    Try JWT option
    - does not like this; wants OAuth2 still

--- publish.py
+++ publish.py
@@ -4,9 +4,12 @@ import base64
 import os
 import requests
 import sys
+import time
+import jwt
 
 # https://developer.chrome.com/docs/webstore/using_webstore_api/
 
+'''
 # curl "https://accounts.google.com/o/oauth2/token" -d \
 # "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$CODE&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
 result = requests.post('https://accounts.google.com/o/oauth2/token',
@@ -27,9 +30,36 @@ import pdb; pdb.set_trace()
 # todo: check that this is working
 result_json = result.json()
 access_token = result_json['access_token']
+'''
 
-# TODO: upload using access_token
-# according to the following CURL:
+iat = time.time()
+exp = iat + 3600
+payload = {'iss': 'chrome-web-store@chrome-web-store-api-gateway.iam.gserviceaccount.com',
+           'sub': 'chrome-web-store@chrome-web-store-api-gateway.iam.gserviceaccount.com',
+           'aud': 'https://www.googleapis.com/auth/chromewebstore',
+           'iat': iat,
+           'exp': exp}
+additional_headers = {'kid': os.environ['CHROME_PRIVATE_KEY_ID']}
+signed_jwt = jwt.encode(payload, os.environ['CHROME_PRIVATE_KEY'], headers=additional_headers,
+                       algorithm='RS256')
+
+
+file_contents = open(sys.argv[1], 'rb').read()
+encoded_contents = base64.b64encode(file_contents)
+payload = encoded_contents
+result = requests.put(f'https://www.googleapis.com/upload/chromewebstore/v1.1/items/{os.environ["CHROME_ITEM_ID"]}',
+                       headers={
+                        'Authorization': f'Bearer {signed_jwt}',
+                        'x-goog-api-version': '2',
+                       },
+                       data=payload)
+
+print(result.text)
+
+if result.status_code != requests.codes.ok:
+    sys.exit(1)
+
+
 '''
 curl \
     -H "Authorization: Bearer $CHROME_ACCESS_TOKEN"  \
@@ -49,15 +79,4 @@ curl \
     https://www.googleapis.com/upload/chromewebstore/v1.1/items/$CHROME_ITEM_ID
 '''
 
-'''
-file_contents = open(sys.argv[1], 'rb').read()
-encoded_contents = base64.b64encode(file_contents)
-payload = encoded_contents
-result = requests.post('https://accounts.google.com/o/oauth2/token',
-                       data=payload)
 
-print(result.text)
-
-if result.status_code != requests.codes.ok or 'failed' in result.json():
-    sys.exit(1)
-'''
