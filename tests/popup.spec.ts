4881662be664fd34789c21c9057639afe6e59df9
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-10-26 20:47:29 -0500

    Rename and clean up popup test

---
+++ tests/popup.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "./fixtures";
+
+test("Popup page has expected buttons", async ({ page, extensionId }) => {
+	await page.goto(`chrome-extension://${extensionId}/pages/index.html`);
+	await expect(page.locator("body")).toContainText("Options");
+});
