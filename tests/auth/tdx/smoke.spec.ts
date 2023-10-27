034f18326adf6fe643e5be38761a93529e445f7f
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-10-26 20:48:08 -0500

    Add todo note in tdx smoke test

--- tests/auth/tdx/smoke.spec.ts
+++ tests/auth/tdx/smoke.spec.ts
@@ -5,4 +5,5 @@ test("Can access TDX", async ({ page }) => {
   //await page.goto(`https://help.uillinois.edu/SBTDNext`);
   await page.goto(BASE_URL);
   // todo follow sign-in etc
+  // and test that actually logged in ok
 });
