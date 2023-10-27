a709e281dd696c92519b03ad4a889b0f3ec9220f
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-10-26 20:45:17 -0500

    Change GH action to only run no-auth playwright tests

---
+++ tests/auth/tdx/smoke.spec.ts
@@ -0,0 +1,8 @@
+import { test } from "../../fixtures";
+import { BASE_URL } from "../../../src/config";
+
+test("Can access TDX", async ({ page }) => {
+  //await page.goto(`https://help.uillinois.edu/SBTDNext`);
+  await page.goto(BASE_URL);
+  // todo follow sign-in etc
+});
